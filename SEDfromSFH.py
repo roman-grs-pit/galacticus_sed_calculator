3d7d7907070973db8d8b1898bed5ade362312112
Andrew-Robertson <arobertson@carnegiescience.edu>
2025-11-20 13:54:34 -0800

    feat: add minimum and maximum wavelength parameters to total spectrum evaluation

--- SEDfromSFH.py
+++ SEDfromSFH.py
@@ -960,12 +960,12 @@ class sed_calculator:
             # Return tuple of (wavelength, flux_density)
             return continuum_wav, total_Fnu
     
-    def evaluate_total_spectrum(self, filename, galIndex, includeAGN=True, obs_wavelengths=np.linspace(8000, 30000, 1000)*u.AA, lineFWHM=10*u.AA, include_emission_lines=True, use_synphot=True):
+    def evaluate_total_spectrum(self, filename, galIndex, includeAGN=True, obs_wavelengths=np.linspace(8000, 30000, 1000)*u.AA, lineFWHM=10*u.AA, include_emission_lines=True, minimumLineFlux=0, minimumLineWavelength = 0.9*u.micron, maximumLineWavelength = 2.03*u.micron, use_synphot=True):
         components=['disk','spheroid']
         if includeAGN:
             components.append('AGN')    
         for i,component in enumerate(components):
-            spectrum = self.evaluate_component_spectrum(filename, galIndex, component=component, obs_wavelengths=obs_wavelengths, lineFWHM=lineFWHM, include_emission_lines=include_emission_lines, use_synphot=use_synphot)
+            spectrum = self.evaluate_component_spectrum(filename, galIndex, component=component, obs_wavelengths=obs_wavelengths, lineFWHM=lineFWHM, include_emission_lines=include_emission_lines, minimumLineFlux=minimumLineFlux, minimumLineWavelength=minimumLineWavelength, maximumLineWavelength=maximumLineWavelength, use_synphot=use_synphot)
             if i==0:
                 total_spectrum = spectrum
             else:
