a547761669130af9b3cbc76670e9cf020584d4e6
Andrew-Robertson <arobertson@carnegiescience.edu>
2025-11-19 18:55:11 -0800

    feat: add use_synphot parameter for totoal_spectrum evaluation

--- SEDfromSFH.py
+++ SEDfromSFH.py
@@ -904,12 +904,12 @@ class sed_calculator:
             # Return tuple of (wavelength, flux_density)
             return continuum_wav, total_Fnu
     
-    def evaluate_total_spectrum(self, filename, galIndex, includeAGN=True, obs_wavelengths=np.linspace(8000, 30000, 1000)*u.AA, lineFWHM=10*u.AA, include_emission_lines=True):
+    def evaluate_total_spectrum(self, filename, galIndex, includeAGN=True, obs_wavelengths=np.linspace(8000, 30000, 1000)*u.AA, lineFWHM=10*u.AA, include_emission_lines=True, use_synphot=True):
         components=['disk','spheroid']
         if includeAGN:
             components.append('AGN')    
         for i,component in enumerate(components):
-            spectrum = self.evaluate_component_spectrum(filename, galIndex, component=component, obs_wavelengths=obs_wavelengths, lineFWHM=lineFWHM, include_emission_lines=include_emission_lines)
+            spectrum = self.evaluate_component_spectrum(filename, galIndex, component=component, obs_wavelengths=obs_wavelengths, lineFWHM=lineFWHM, include_emission_lines=include_emission_lines, use_synphot=use_synphot)
             if i==0:
                 total_spectrum = spectrum
             else:
